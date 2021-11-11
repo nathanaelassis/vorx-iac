f3cbe88bddc065f6e1231d1127e32b6f3e846e9f
Nathanael Assis <39883733+nathanaelassis@users.noreply.github.com>
2021-11-11 20:52:01 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "bucket_name" {
-    type = string
-    description = "Informe o nome do Bucket criado para armazenar os arquivos tfstate"
-}
+#variable "bucket_name" {
+#    type = string
+#    description = "Informe o nome do Bucket criado para armazenar os arquivos tfstate"
+#}
